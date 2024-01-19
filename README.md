a1c6dc8833067949d387bf8a70d987f455a2cf83
Андрей Петров <petrov_av@tochka.com>
2024-01-19 20:54:37 +0400

    Исправлено отображение в компоненте App Дополнено README

--- README.md
+++ README.md
@@ -11,3 +11,5 @@
 _**Дизайн:**_ https://www.figma.com/file/gItXdLAvDaPdVp7yh4mtgi/%D0%A2%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D0%BE%D0%B5-%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5.-%D0%92%D0%B5%D1%80%D1%81%D1%82%D0%BA%D0%B0?node-id=0%3A1&t=k9j4HEkgXnj5JDoY-1
 
 _**Резервная ссылка на дизайн:**_ https://www.figma.com/community/file/1330216302448835305/adsensor
+
+**GitHub Pages:** https://andreysfn.github.io/test-task-adsensor/
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css'
-import {Card} from "./atoms";
-import {RecommendationPage} from "./pages/recommendation.page.tsx";
+import {RecommendationPage} from "./pages";
 
 function App() {
     return (
         <>
-            <RecommendationPage>
-                <Card text='Проверка настройки лимитов бюджета, работу со ставками и динамика расходов' title='Бюджет'
-                      number={94} sideColor='#F14B4B'/>
-            </RecommendationPage>
+            <RecommendationPage/>
         </>
     )
 }
